6f39a7d7488efe1442e5b85afc414e345c8fc78a
Hennie Bruurmijn <hlm.bruurmijn@gmail.com>
2022-06-09 18:55:43 +0200

    Add .rb file with basic Node class

---
+++ lib/logic_theory_machine.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+# Class used to create a linked list
+class LinkedList
+
+end
+
+# Class used for every node in the linked list
+class Node
+  def initialize(value = nil, next_node = nil)
+    @value = value
+    @next_node = next_node
+  end
+
+  attr_accessor :value, :next_node
+end
