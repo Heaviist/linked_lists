2d3533d0ea0095ed2da17586bd201df980a5e360
Hennie Bruurmijn <hlm.bruurmijn@gmail.com>
2022-06-12 15:17:56 +0200

    Add #contains?

--- lib/logic_theory_machine.rb
+++ lib/logic_theory_machine.rb
@@ -3,6 +3,7 @@
 # Class used to create a linked list
 ## Consider adding an @tail instance variable. Downside is keeping @tail in memory, without pointers from back to front, I don't see it having much value, since you would have to iterate to that point every time anyway. #pop for example would still have to change the previous node's next variable to nil.
 ## Consider letting pop return the deleted value.
+### Consider adding an instance variable @size that keeps track of the number of entries in the list. There are obvious downsides to this though.
 class LinkedList
   attr_accessor :head
 
@@ -62,6 +63,18 @@ class LinkedList
       current_node.next = nil
     end
     last_node
+  end
+
+  def contains?(value)
+    return false if empty?
+
+    current_node = @head
+    until current_node.next.nil?
+      return true if current_node.value == value
+
+      current_node = current_node.next
+    end
+    false
   end
 
   def clear
